3d215e9552edce67c7b0e54f7364e2382451dd98
Nil E <nil.erdenn@gmail.com>
2024-03-30 16:35:14 +0100

    iteration 1 is done

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,41 @@
 const router = require("express").Router();
+const bcryptjs = require('bcryptjs');
+const User = require('../models/User.model');
+const saltRounds = 10;
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+router.get("/signup", (req, res, next) => {
+  res.render("auth/signup");
+});
+
+router.post("/signup", (req, res, next) => {
+  console.log('The form data: ', req.body);
+  const { username, password } = req.body;
+  bcryptjs
+  .genSalt(saltRounds)
+  .then(salt => bcryptjs.hash(password, salt))
+  .then(hashedPassword => {
+    return User.create({
+      username,
+      passwordHash: hashedPassword
+    });
+  })
+  .then(userFromDB => {
+    console.log('Newly created user is: ', userFromDB);
+    res.redirect('/userProfile');
+})
+  .catch(error => next(error));
+  res.redirect('/userprofile');
+});
+
+router.get("/userprofile", (req, res, next) => {
+  res.render("users/userprofile");
+});
+
+
 
 module.exports = router;
