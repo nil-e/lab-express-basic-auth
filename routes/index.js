b444e047d7776dde27bb1153ad83a1d15eaafab2
Nil E <nil.erdenn@gmail.com>
2024-03-31 21:22:43 +0200

    iteration 1 is done, iteration 2 is in progress. i need to stop logging in if the password is not correct. wip

--- routes/index.js
+++ routes/index.js
@@ -14,14 +14,15 @@ router.get("/signup", (req, res, next) => {
 
 router.post("/signup", (req, res, next) => {
   console.log('The form data: ', req.body);
-  const { username, password } = req.body;
+   const { username, password } = req.body;
+  console.log('username', username, 'pwd', password);
   bcryptjs
   .genSalt(saltRounds)
   .then(salt => bcryptjs.hash(password, salt))
   .then(hashedPassword => {
     return User.create({
       username,
-      passwordHash: hashedPassword
+      password: hashedPassword
     });
   })
   .then(userFromDB => {
@@ -35,6 +36,29 @@ router.get("/userprofile", (req, res, next) => {
   res.render("users/userprofile");
 });
 
+router.get("/login", (req, res, next) => {
+  res.render("auth/login");
+});
 
+router.post("/login", (req, res, next) => {
+  console.log('The form data: ', req.body);
+   const { username, password } = req.body;
+  console.log('username', username, 'pwd', password);
+  bcryptjs
+  .genSalt(saltRounds)
+  .then(salt => bcryptjs.hash(password, salt))
+  .then(hashedPassword => {
+    let userFound = User.find({
+      username: username,
+      passwordHash: hashedPassword
+    });
+    return console.log(userFound);
+  })
+  .then(userFromDB => {
+    console.log('We can log in:', userFromDB);
+    res.redirect('/userProfile');
+})
+  .catch(error => next(error));
+});
 
 module.exports = router;
